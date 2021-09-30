af9a5230f9a7634f3296760d507b0ed058c23102
mousedrool <richard.abrahamson@yahoo.com>
2021-09-30 09:39:13 -0500

    reimplementation to switch to basic authentication

--- src/main/java/com/rca/aurhotization/azureb2ctokenvalidator/config/WebSecurity.java
+++ src/main/java/com/rca/aurhotization/azureb2ctokenvalidator/config/WebSecurity.java
@@ -1,23 +1,33 @@
 package com.rca.aurhotization.azureb2ctokenvalidator.config;
 
-import com.rca.aurhotization.azureb2ctokenvalidator.filters.AuthorizeRequestFilter;
+import com.rca.aurhotization.azureb2ctokenvalidator.authorization.BasicAuthenticationProvider;
+import com.rca.aurhotization.azureb2ctokenvalidator.authorization.CredentialsStore;
+import com.rca.aurhotization.azureb2ctokenvalidator.utilities.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(authorizeRequestFilter(), BasicAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+    @Autowired
+    CredentialsStore credentialsStore;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(new BasicAuthenticationProvider(credentialsStore));
     }
 
-    private AuthorizeRequestFilter authorizeRequestFilter() { return new AuthorizeRequestFilter();}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (System.getenv(Constants.ENV_BASIC_SECURITY) != null) {
+            http.httpBasic().
+                    and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                    and().csrf().disable().
+                    authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
+        }
+    }
 }
