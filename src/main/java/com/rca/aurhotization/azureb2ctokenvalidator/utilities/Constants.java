f7fb07dd94531f665b539c8ec85ad6e41cba3cc5
mousedrool <richard.abrahamson@yahoo.com>
2021-09-30 09:37:28 -0500

    switch implementation to single json based configuration

--- src/main/java/com/rca/aurhotization/azureb2ctokenvalidator/utilities/Constants.java
+++ src/main/java/com/rca/aurhotization/azureb2ctokenvalidator/utilities/Constants.java
@@ -2,7 +2,6 @@ package com.rca.aurhotization.azureb2ctokenvalidator.utilities;
 
 public class Constants {
 
-    public static String ENV_KEY = "AUTH_KEY_KEY";
-    public static String ENV_SECRET = "AUTH_KEY_SECRET";
+    public static String ENV_BASIC_SECURITY = "BASIC_SECURITY";
 
 }
