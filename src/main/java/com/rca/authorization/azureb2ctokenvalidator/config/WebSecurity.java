8fc074da5c541bad52d1773f3474ca4b852c973f
mousedrool <richard.abrahamson@yahoo.com>
2021-10-12 14:51:23 -0500

    add new wakeup endpoint which should be unsecured.

--- src/main/java/com/rca/authorization/azureb2ctokenvalidator/config/WebSecurity.java
+++ src/main/java/com/rca/authorization/azureb2ctokenvalidator/config/WebSecurity.java
@@ -4,12 +4,18 @@ import com.rca.authorization.azureb2ctokenvalidator.authorization.BasicAuthentic
 import com.rca.authorization.azureb2ctokenvalidator.authorization.CredentialsStore;
 import com.rca.authorization.azureb2ctokenvalidator.utilities.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -28,10 +34,25 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         if (environment.containsProperty(Constants.ENV_BASIC_SECURITY)) {
-            http.httpBasic().
+            http.cors().and().httpBasic().
                     and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
                     and().csrf().disable().
-                    authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
+                    authorizeRequests()
+                    .antMatchers("/wakeup").permitAll()
+                    .antMatchers("/validate").permitAll().anyRequest().authenticated();
         }
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
 }
